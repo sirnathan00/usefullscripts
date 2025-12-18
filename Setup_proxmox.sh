51a1fe890e4e94c525a8045ff590af6e1abeb776
sirnathan00 <sirnathan00@aol.com>
2025-12-18 19:35:24 +0100

    commit

--- Setup_proxmox.sh
+++ Setup_proxmox.sh
@@ -8,7 +8,7 @@ cp Public_homelab_linux /root/.ssh/authorized_keys
 
 sed -i 's/#PubkeyAuthentication no/PubkeyAuthentication yes/'  /etc/ssh/sshd_config
 sed -i 's|#AuthorizedKeysFile      .ssh/authorized_keys .ssh/authorized_keys2|AuthorizedKeysFile      .ssh/authorized_keys .ssh/authorized_keys2|' /etc/ssh/sshd_config
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+#sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 
 systemctl restart ssh sshd
 
